7b79f147b103c4d4f3cafd29e62d01c273972ac7
Admin <lex.meins@proton.me>
2026-02-05 19:03:04 +0500

    fully recreated background

--- main_scene.gd
+++ main_scene.gd
@@ -1,18 +1,18 @@
 extends Node2D
 
 @export var flower_scene: PackedScene
-@onready var background: AnimatedSprite2D = $Background
 
 var grass: Array[Node2D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	background.play("default")
 	var grassObjects = $Grass.get_children()
 	for k in grassObjects:
 		grass.append(k as Node2D)
-	var enemy = flower_scene.instantiate()
 	grass.reverse()
+
+	var enemy = flower_scene.instantiate()
+	enemy.z_index = 10
 	enemy.set_grass(grass)
 	add_child(enemy)
 	
